8961c1ff1ebb1856bff636172826cf339f859e95
Philip Louie Templonuevo <philiplouietemplonuevo@yahoo.ca>
2018-03-01 04:34:24 +0000

    Clean-up. Added form validation, refetch tweets, cross-site scripting and form toggle.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,86 +3,90 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+
 $(document).ready(function() {
+  // Creates each tweet and return to be rendered into the site
+  function createTweetElement(tweetObj) {
+    const $tweet = $("<article>").addClass("tweet");
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461116232227
+    const $header = $("<header>");
+    const $name = $("<h1>").text(tweetObj.user.name);
+    const $handle = $("<p>").text(tweetObj.user.handle);
+
+    $header.append(`<img alt="avatar" src="${tweetObj.user.avatars.regular}" />`);
+    $header.append($name);
+    $header.append($handle);
+    $header.append(`<div class="clearfix"></div>`);
+
+    const $main = $("<main>");
+    const $mainContent = $("<p>").text(tweetObj.content.text);
+    $main.append($mainContent);
+
+    const $footer = $("<footer>");
+    const $date = $("<p>").text(`${tweetObj.created_at} day(s) ago`);
+    $footer.append($date);
+
+    // Add icons
+    $footer.append(`<div class="icons"><i class="fas fa-flag"></i> <i class="fas fa-retweet"></i> <i class="fas fa-heart"></i></div>`);
+    $footer.append(`<div class="clearfix"></div>`);
+
+    $tweet.append($header);
+    $tweet.append($main);
+    $tweet.append($footer);
+
+    return $tweet;
   }
-];
 
+  // Renders the tweets
+  function renderTweets(tweets) {
+    tweets.forEach(function(tweetObj) {
+      const $tweet = createTweetElement(tweetObj);
+      // takes return value and prepends it to the tweets container
+      $("#tweets-container").prepend($tweet);
+    });
+  }
 
-function createTweetElement(tweetObj) {
-  const $tweet = $('<article>').addClass("tweet");
-  const $header = $("<header>");
-  const $name = $("<h1>").text(tweetObj.user.name);
-  const $handle = $("<p>").text(tweetObj.user.handle);
-  const $main = $("<main>");
-  const $content = $("<p>").text(tweetObj.content.text);
-  const $footer = $("<footer>");
-  const $date = $('<p>').text(`${daysAgo} days ago`);
-  const daysAgo = getDaysAgo(tweetObj.created_at);
-  $tweet.append($header);
-  $tweet.append($main);
-  $tweet.append($footer);
-  $header.prepend(`<img alt="avatar" src="${tweetObj.user.avatars.regular}" />`);
-  $header.append($name);
-  $header.append($handle);
-  $header.append('<div class="clearfix"></div>');
-  $main.append($content);
-  $footer.append($date);
+  // Fetchs tweets from /tweets
+  function loadTweets() {
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+      success: function (tweetsJSON) {
+        renderTweets(tweetsJSON);
+      }
+    });
+  }
 
-  return $tweet;
-}
+  // Get the data after submitted from the new tweet form
+  $(".new-tweet form").on("submit", function(event) {
+    // Prevent the default behaviour
+    event.preventDefault();
+    // Get the data from the form
+    const data = $(".new-tweet form").serialize();
+    // Checks if empty or with more then 140 characters and alert the user (145 because of the 'text=' that exist in the data)
+    if (data === "text=") {
+      alert(`Sorry! You can't submit a empty tweet!`);
+    } else if (data.length > 145) {
+      alert(`Sorry! You can't submit a tweet with more the 140 characters!`);
+    } else {
 
-function renderTweets(tweets) {
-  tweets.forEach(function(tweetObj) {
-    const $tweet = createTweetElement(tweetObj);
-    $('#tweets-container').append($tweet);
+      // Submit using AJAX
+      $.ajax({
+        url: "/tweets",
+        method: "POST",
+        data: data,
+        success: function (result) {
+          // Empties the tweets and loads all tweets again (with the new one sent to database)
+          $("#tweets-container").empty();
+          $(".new-tweet form span").text(140);
+          $(".new-tweet form textarea").val("");
+          loadTweets();
+        }
+      });
+    }
+
   });
-}
 
-renderTweets(data);
+  loadTweets();
 
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,21 +1,23 @@
-$(document).ready(function () {
-  $('.new-tweet').on('keyup', 'textarea', function() {
-    // console.log('length: ', this);
-    var maxLength = 140;
-    var tweetLength = $(this).val().length;
-    var finalLength = maxLength - tweetLength;
-    if (tweetLength > maxLength) {
-      $(this).siblings('.counter').addClass('red');
-      $(this).siblings('.counter').text(finalLength);
+$(document).ready(function() {
+
+  // Gets the input in the textarea and counts the characters.
+  $(".new-tweet form textarea").on("keyup", function() {
+    const text  = $(this).val();
+
+    // Sets counter of remaining characters and red if negative (and returns black if returns to be positive)
+    if ((140 - text.length) < 0) {
+      $(this).parent().children("span").text(140 - text.length).css("color", "red");
     } else {
-      $(this).siblings('.counter').removeClass('red');
-      $(this).siblings('.counter').text(finalLength);
+      $(this).parent().children("span").text(140 - text.length).css("color", "black");
     }
   });
+
+  // Sets the nav-bar button ("Compose") as toggle to slide showing and hidding the new-tweet form
+  $( function() {
+    $( "#nav-bar button" ).click(function() {
+      $( ".new-tweet").slideToggle( );
+      $( ".new-tweet form textarea").focus();
+    });
+  });
+  $( ".new-tweet").hide();
 });
-
-
-
-
-
-
